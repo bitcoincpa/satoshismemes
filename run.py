b57e4be671780e868fcd4972a54e55fc21042da3
Dave Mag <davemagarian@Daves-MacBook-Pro-2.local>
2019-10-25 22:24:05 -0700

    procfile

--- run.py
+++ run.py
@@ -6,3 +6,4 @@ APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 if __name__ == '__main__':
      app.run(debug=True)
+
--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,6 @@
+from whitenoise import WhiteNoise
+
 from app import app
 
-app = app()
-
+application = WhiteNoise(app)
+application.add_files('static/', prefix='static/')
