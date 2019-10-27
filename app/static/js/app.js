2786e8282ea9c647df07e752cbd882a516440d7c
Dave Mag <davemagarian@Daves-MacBook-Pro-2.local>
2019-10-27 12:48:23 -0700

    fix

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -9,12 +9,24 @@ $('#submit').click(function() {
         processData: false,
         dataType: 'json'
     }).done(function(data, textStatus, jqXHR){
-        $('#ItemPreview').attr('src', 'data:image/png;base64,' + data['byte']);
-        $("#adv-btn").show();
-    }).fail(function(data){
+        console.log(data);
+        console.log(textStatus);
+        console.log(jqXHR);
+        console.log('Success!');
+        //$("#resultFilename").text(data['name']); // name of the file
+        //$("#resultFilesize").text(data['size']); // size of the file
+        //$("#resultCaption").text(data['caption']); // name of the file
+        //$("#resultTag").text(data['tag']); // size of the file
+        //my_url = "{{ url_for('upload', filename="+data['name']+" }}";
+        //$("#img").attr('src', '/static/'+data['path']);
+        //$("#meme").attr('src', '/static/'+data['meme']);
         $('#ItemPreview').attr('src', 'data:image/png;base64,' + data['byte']);
         //$("#image").attr('src', data['path']); // setting the src attribute of img tag
-        //$("#adv-btn").attr('value', data['name']);
+        $("#adv-btn").attr('value', data['name']);
         $("#adv-btn").show();
+
+        //get_image(data['name'])
+    }).fail(function(data){
+        alert('Choose a valid image file.');
     });
 });
